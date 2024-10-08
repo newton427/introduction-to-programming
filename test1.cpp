d9725cc4506f1fcfd97a91b65ab08262faef7a68
newton427 <orchnewton@gmail.com>
2024-10-08 18:29:55 +0300

    status

---
+++ test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int number;
+    cout << "Enter a  number"; //prompt user input
+    cin >> number ; // capture input
+    cout << "You entered" + number << endl;  //capture input
+    cerr<< "This is an example of an error message";
+}
