db6f2887ffe31b04987ca1912c601f4143e712af
newton427 <orchnewton@gmail.com>
2024-10-15 19:28:40 +0300

    status

---
+++ escape sequence1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Escape Sequence Practice" << endl;
+    cout << "*****" << endl;
+    cout << "BBIT is a university diploma designed to focus on:" << endl;
+    cout << "\t1. Development" << endl;    // Tab space before each point
+    cout << "\t2. Implementation," << endl; // Tab space before each point
+    cout << "\t3. Management of information" << endl; // Tab space before each point
+    return 0;
+}
+
+
--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main()
 {
-    cout << "Hello World. Glad to begin learning C++";
-    cout << "My name is Newton Morrison";
+    cout << "Hello World. Glad to begin learning C++\n";
+    cout << "My name is Newton Morrison\n";
+    cout << "Welcome to the wonderful world of C++\n";
+    cout << "This is my first C++ output statement\n";
+    cout << "Stratmore University is the leading \n";
+    cout << "University in the region";
     return 0;
 }
