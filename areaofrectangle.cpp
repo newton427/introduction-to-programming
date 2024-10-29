1f0897099920c11ff8324106d875305ac1c2019f
newton427 <orchnewton@gmail.com>
2024-10-29 19:07:08 +0300

    cmt

---
+++ areaofrectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable declaration
+    float area, length, width;
+    
+    // Variable initialization
+    length = 8;
+    width = 5; 
+    
+    // Computing the area
+    area = length * width;
+    
+    // Output the area
+    cout << "Area of the rectangle is: " << area << endl;
+    
+    return 0;
+}
--- escapesequence2.cpp
+++ escapesequence2.cpp
@@ -1,9 +1,2 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "        O     O       \n"; 
-    cout << "       
-    cout << 
-    cout <<
+#
 
