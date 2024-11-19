9f352774702dcb9660a1a02241f5d2df6b919b5f
newton427 <orchnewton@gmail.com>
2024-11-19 20:19:11 +0300

    status

---
+++ forloop3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int num = 12; num <= 50; num += 2) { // Start at 12, increment by 2
+        cout << num << " "; // Output the current even number
+    }
+
+    cout << endl; // End with a new line for better formatting
+    return 0;
+}
